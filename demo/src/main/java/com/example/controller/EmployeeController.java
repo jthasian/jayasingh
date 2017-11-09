166dea7bbe5b664c99f00b3648713d3b879e6357
jthasian <jthasian@192.168.0.10>
2017-11-09 18:29:34 +0100

    swagger

--- demo/src/main/java/com/example/controller/EmployeeController.java
+++ demo/src/main/java/com/example/controller/EmployeeController.java
@@ -11,17 +11,33 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.model.Employee;
 import com.example.service.EmployeeService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
+@Api(value="userservice", description="user service detail")
 public class EmployeeController {
 	
 	@Autowired
 	private EmployeeService employeeService;
 	
+	@ApiOperation(value = "View a list of available users",response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
     @RequestMapping(value = "/employee", method = RequestMethod.GET)
     public List<Employee> getEmployees() {
 		return employeeService.getAllEmployees();
 	}
 
+	@ApiOperation(value = "Search a user with an ID",response = Employee.class)
     @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
     public Employee getEmployee(@PathVariable("id") long id) {
 		return employeeService.getEmployeeById(id);
